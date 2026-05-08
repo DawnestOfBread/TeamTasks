98765b723d5245ba73a35f18074c08d032597103
DawnestOfBread <81489726+DawnestOfBread@users.noreply.github.com>
2026-05-08 10:15:53 +0200

    fix: update migrations

---
+++ TeamTasks.Infrastructure/Migrations/20260507094011_AddExternalAuth.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeamTasks.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExternalAuth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ExternalId",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Provider",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExternalId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Provider",
+                table: "Users");
+        }
+    }
+}
