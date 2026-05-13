bcb28373ff9deb4124354f172610d9150517d9ed
DawnestOfBread <81489726+DawnestOfBread@users.noreply.github.com>
2026-05-13 10:31:22 +0200

    fix: update migrations

---
+++ TeamTasks.Infrastructure/Migrations/20260512102745_FixCollections.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeamTasks.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixCollections : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Organizations_OrganizationId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_OrganizationId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "OrganizationId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "OrganizationIds",
+                table: "Users");
+
+            migrationBuilder.CreateTable(
+                name: "UserOrganizations",
+                columns: table => new
+                {
+                    OrganizationsId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UsersId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserOrganizations", x => new { x.OrganizationsId, x.UsersId });
+                    table.ForeignKey(
+                        name: "FK_UserOrganizations_Organizations_OrganizationsId",
+                        column: x => x.OrganizationsId,
+                        principalTable: "Organizations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserOrganizations_Users_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserOrganizations_UsersId",
+                table: "UserOrganizations",
+                column: "UsersId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserOrganizations");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "OrganizationId",
+                table: "Users",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<List<Guid>>(
+                name: "OrganizationIds",
+                table: "Users",
+                type: "uuid[]",
+                nullable: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_OrganizationId",
+                table: "Users",
+                column: "OrganizationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Organizations_OrganizationId",
+                table: "Users",
+                column: "OrganizationId",
+                principalTable: "Organizations",
+                principalColumn: "Id");
+        }
+    }
+}
