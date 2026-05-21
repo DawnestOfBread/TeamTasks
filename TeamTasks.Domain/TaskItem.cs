e5b6f361f79875b2c542aebe9fcf322955c313fc
DawnestOfBread <81489726+DawnestOfBread@users.noreply.github.com>
2026-05-21 13:08:15 +0200

    fix: increase task description length

--- TeamTasks.Domain/TaskItem.cs
+++ TeamTasks.Domain/TaskItem.cs
@@ -6,7 +6,7 @@ public class TaskItem
 {
 	public Guid Id { get; set; }
 	[MaxLength(64)] public string Title { get; set; }
-	[MaxLength(255)] public string Description { get; set; }
+	[MaxLength(512)] public string Description { get; set; }
 	public TaskStatus Status { get; set; }
 	public Guid ProjectId { get; set; }
 	public Guid? AssignedUserId { get; set; }
---
+++ TeamTasks.Infrastructure/Migrations/20260521110741_UpdateDescLength.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeamTasks.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDescLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Tasks",
+                type: "character varying(512)",
+                maxLength: 512,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(255)",
+                oldMaxLength: 255);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Tasks",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(512)",
+                oldMaxLength: 512);
+        }
+    }
+}
